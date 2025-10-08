ec3fcc585fe06e10f09ed450c3fee93c95bb0e3b
noskcaj1 <jackson.sbm@gmail.com>
2025-10-07 22:18:22 -0300

    Adiciona arquivo variaveis.java

---
+++ variaveis.java
@@ -0,0 +1,15 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String nome = "Naruto Uzumaki"; //Variável do tipo String para armazenar o nome do ninja
+        int idade = 16; //Variável do tipo int para armazenar a idade do ninja
+        double altura = 1.65; //Variável do tipo double para armazenar a altura do ninja
+        String aldeia = "Folha"; //Variável do tipo String para armazenar a aldeia do ninja
+        boolean vivo = true; //Variável do tipo boolean para armazenar se o ninja está vivo ou não
+
+        System.out.println("Nome do Ninja: " + nome); //Imprime o nome do ninja
+        System.out.println("Idade: " + idade + " anos de idade"); //Imprime a idade do ninja
+        System.out.println("Altura: " + altura + "m"); //Imprime a altura do ninja
+        System.out.println("A aldeia do ninja: " + nome + " é a " + aldeia); //Imprime a aldeia do ninja
+        System.out.println("O ninja " + nome + " ainda está vivo? " + vivo); //Imprime se o ninja está vivo ou não
+    }
+}
